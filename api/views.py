ca83bcfe6fa8388f8a01a224b14ff8b4f433f36d
akramelassri <168238434+akramelassri@users.noreply.github.com>
2026-05-12 10:16:19 +0100

    Add student list view, and add url

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from .models import Student
+
+def student_list(request):
+    students = Student.objects.all()
+
+    data = []
+
+    for student in students:
+        data.append({
+            "id": student.id,
+            "title": student.title,
+            "author": student.author,
+            "published_year": student.published_year
+        })
+
+    return JsonResponse(data, safe=False)
